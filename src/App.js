15a056c74a8bd21952e3cf06264b356851b8379c
MRJAHID <mrjahid350@gmail.com>
2022-08-21 12:51:15 +0600

    API Call

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
+import React, { useEffect } from "react";
 import Header from "./components/Header/Header";
 import List from "./components/List/List";
 import Map from "./components/Map/Map";
 import { CssBaseline, Grid } from "@material-ui/core";
+import { getPlacesData } from "./Api";
 
 function App() {
+  const [type, setType] = useState("restaurants");
+  const [rating, setRating] = useState("");
+
+  const [coords, setCoords] = useState({});
+  const [bounds, setBounds] = useState(null);
+
+  const [weatherData, setWeatherData] = useState([]);
+  const [filteredPlaces, setFilteredPlaces] = useState([]);
+  const [places, setPlaces] = useState([]);
+
+  const [autocomplete, setAutocomplete] = useState(null);
+  const [childClicked, setChildClicked] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    getPlacesData().then((data) => {
+      setPlaces(data);
+    });
+  }, []);
   return (
     <>
       <CssBaseline />
@@ -13,7 +34,14 @@ function App() {
           <List />
         </Grid>
         <Grid item xs={12} md={8}>
-          <Map />
+          <Map
+            setChildClicked={setChildClicked}
+            setBounds={setBounds}
+            setCoords={setCoords}
+            coords={coords}
+            places={filteredPlaces.length ? filteredPlaces : places}
+            weatherData={weatherData}
+          />
         </Grid>
       </Grid>
     </>
