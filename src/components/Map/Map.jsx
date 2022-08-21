d462d04778b8a6ca663cbc12548d7655fe1e407c
MRJAHID <mrjahid350@gmail.com>
2022-08-21 12:08:29 +0600

    Map Show

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,13 +1,36 @@
-import React from 'react';
-import './style.js'
-
+import React from "react";
+import useStyles from "./style.js";
+import { useMediaQuery } from "@material-ui/core";
+import GoogleMapReact from "google-map-react";
 
 const Map = () => {
-    return (
-        <div>
-            Map
-        </div>
-    );
+  // const matches = useMediaQuery("(min-width:600px)");
+  const classes = useStyles();
+  const coordinates = { lat: 0, lng: 0 };
+
+  return (
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "AIzaSyBcEkDnev3Y6q7njU8A5YEq2ieszZxr0Pc" }}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        // options={{
+        //   disableDefaultUI: true,
+        //   zoomControl: true,
+        //   styles: mapStyles,
+        // }}
+        // onChange={(e) => {
+        //   setCoords({ lat: e.center.lat, lng: e.center.lng });
+        //   setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw });
+        // }}
+        // onChildClick={(child) => setChildClicked(child)}
+      >
+        {" "}
+      </GoogleMapReact>
+    </div>
+  );
 };
 
 export default Map;
